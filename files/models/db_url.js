37df0cc81926223c6c86c5f11af6c433350265b5
Omar Ouz <84501723+oz-om@users.noreply.github.com>
2023-01-13 22:55:32 +0100

    Update db

--- files/models/db_url.js
+++ files/models/db_url.js
@@ -1,5 +1,5 @@
 // const db_url = "mongodb://127.0.0.1:27017/chat";
-const db_url = "mongodb+srv://root:root@app-chat.4eqqw.mongodb.net/chat?retryWrites=true&w=majority";
+const db_url = "mongodb+srv://root:root@chat-db.lonx4.mongodb.net/chat-app?retryWrites=true&w=majority";
 // const db_url = "mongodb://192.68.43.13";
 
 module.exports = db_url
